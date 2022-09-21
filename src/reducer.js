743d3f1a52630dbff302d5f001387a7f0939646a
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2022-09-21 15:53:10 +0300

    removed unnecessary comment

--- src/reducer.js
+++ src/reducer.js
@@ -77,10 +77,3 @@ function reducer(state = initialStore, action) {
 }
 
 export default reducer;
-
-// switch (action.type) {
-//   case CLEAR_CART:
-//     return { ...state, cart: [] };
-//   default:
-//     return state;
-// }
